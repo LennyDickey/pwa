b5f98a7c77296d85a1e901665a3176696d80984f
Lenny Dickey <Len.dickey@protonmail.com>
2019-11-23 21:08:46 -0500

    update

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const logger = require('morgan')
-const mongoose = require('mongoose')
-const usersRoutes = require('./routes')
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const usersRoutes = require("./routes");
 
-const app = express()
+const app = express();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/auth'
-const PORT = process.env.PORT || 3001
+const MONGODB_URI = process.env.MONGODB_URI;
+// || 'mongodb://localhost/auth'
+const PORT = process.env.PORT || 3001;
 
-mongoose.set('useCreateIndex', true)
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
-	console.log(err || `Connected to MongoDB.`)
-})
+mongoose.set("useCreateIndex", true);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, err => {
+  console.log(err || `Connected to MongoDB.`);
+});
 
-app.use(express.static(`${__dirname}/client/build`))
-app.use(logger('dev'))
-app.use(express.json())
+app.use(express.static(`${__dirname}/client/build`));
+app.use(logger("dev"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api', (req, res) => {
-	res.json({message: "API root"})
-})
+app.get("/api", (req, res) => {
+  res.json({ message: "API root" });
+});
 
-app.use('/api/users', usersRoutes)
+app.use("/api/users", usersRoutes);
 
-app.use('*', (req, res) => {
-	res.sendFile(`${__dirname}/client/build/index.html`)
-})
+app.use("*", (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`);
+});
 
-app.listen(PORT, (err) => {
-	console.log(err || `Server running on port ${PORT}.`)
-})
+app.listen(PORT, err => {
+  console.log(err || `Server running on port ${PORT}.`);
+});
